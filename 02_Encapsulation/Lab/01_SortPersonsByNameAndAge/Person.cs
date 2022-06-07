2b1473d959b038d897004349630cdf836566e359
GLidia <74815962+GLidia@users.noreply.github.com>
2022-06-07 11:36:21 +0300

    Add lesson 2 lab

---
+++ 02_Encapsulation/Lab/01_SortPersonsByNameAndAge/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            private set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            private set { this.lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set { 
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative");
+                }
+                this.age = value; }
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {           
+            return $"{this.firstName} {this.lastName} is {this.age} years old.";                 
+        }
+    }
+}
---
+++ 02_Encapsulation/Lab/02_SalaryIncrease/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            private set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            private set { this.lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set { 
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative");
+                }
+                this.age = value; }
+        }
+
+        public decimal Salary
+        {
+            get { return this.salary; }
+            private set { this.salary = value; }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age < 30)
+            {
+                percentage /= 2;
+            }
+
+            this.salary += this.salary * percentage /100;
+        }
+        public override string ToString()
+        {            
+            return $"{this.firstName} {this.lastName} receives {this.salary:F2} leva.";                      
+        }
+    }
+}
