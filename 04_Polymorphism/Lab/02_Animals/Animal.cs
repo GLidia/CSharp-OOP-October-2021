69f50ded764c185641008fe8831f429c630f76ca
GLidia <74815962+GLidia@users.noreply.github.com>
2022-06-09 20:53:01 +0300

    Add lesson 4 lab

---
+++ 04_Polymorphism/Lab/02_Animals/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        public string Name
+        {
+            get { return this.name; }
+            protected set { this.name = value; }
+        }
+
+        public string FavoriteFood
+        {
+            get { return this.favoriteFood; }
+            protected set { this.favoriteFood = value; }
+        }
+
+        protected Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favoriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my favorite food is {this.favoriteFood}";
+        }
+    }
+}
---
+++ 04_Polymorphism/Lab/03_Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public double Height
+        { 
+            get { return this.height; } 
+            private set { this.height = value; }
+        }
+
+        public double Width
+        {
+            get { return this.width; }
+            private set { this.width = value; }
+        }
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return this.height * 2 + this.width * 2;
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
